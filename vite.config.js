a97a8453b8f31e62917ecf04e78c6fbf75d535e6
zulfikarelreal <zulkfikarelreal@gmail.com>
2026-06-05 18:04:58 +0700

    update

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,8 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
 
 export default defineConfig({
   plugins: [react()],
-  base: '/portofolio-zulfikarelreal/',
-})
+  base:
+    process.env.NODE_ENV === "production" ? "/portofolio-zulfikarelreal/" : "/",
+});
